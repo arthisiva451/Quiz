8ee35388c3fbd5046c4e17ace6d3844410006cf7
arthisiva451 <66836097+arthisiva451@users.noreply.github.com>
2020-06-21 11:12:43 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,7 @@
+var questions = [
+  ["The basic difference between JavaScript and Java is _________", "There is no difference", "Functions are considered as fields", "Variables are specific"," Functions are values, and there is no hard distinction between methods and fields", "4"],
+  ["Which variables are used internally in object methods and are also globally visible?", "Object properties", "Variable properties", "Method properties","Internal properties","2"],
+ ["The class that represents the regular expressions is ________", "RegExpObj", " RegExpClass", "RegExp","StringExp","3"],
+ ["You can refresh the webpage in JavaScript by using ________", " window.reload", "location.reload", "window.refresh","page.refresh","2"],
+ ["What kind of scoping does JavaScript use?", "Literal", " Lexical", "Segmental","Sequential","2"],
+  ];
